9252a85422bc4a36f4ff015b0b0d94f6da5f0701
Pawsing-ctr <tnechayev@internet.ru>
2025-01-17 22:17:58 +0300

    make el list, ArrowDown key work

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { IMaskInput } from "react-imask";
 import { z } from "zod";
 import "./App.css";
-
-// const schemaErrorOptional = { required_error: "Обязательное поле" };
+import { inputText } from "./const/inputConst";
 
 const userSchema = z.object({
   userName: z.string().min(1, "Обязательное поле"),
@@ -26,6 +25,17 @@ const newUser = {
 const App = () => {
   const [newUserObject, setNewUserObject] = useState(newUser);
   const [errors, setErrors] = useState({});
+  const [isActive, setIsActive] = useState(0);
+  console.log(isActive);
+
+  const activeStyle = "active";
+
+  useEffect(() => {
+    window.addEventListener("keydown", keydownFunctions);
+    return () => {
+      window.removeEventListener("keydown", keydownFunctions);
+    };
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -43,69 +53,127 @@ const App = () => {
     }
   };
 
+  const keydownFunctions = (event) => {
+    if (event.key === "ArrowDown") {
+      setIsActive((prevIndex) =>
+        // prevIndex < 20 - 1 ? prevIndex + 1 : prevIndex === 1
+        {
+          if (prevIndex <= 20) {
+            prevIndex += 1;
+          }
+          if (prevIndex > 20) {
+            prevIndex = 1;
+          }
+        }
+      );
+    }
+  };
+
+  const goida = (id) => {
+    setIsActive(id);
+  };
+
   return (
-    <div>
-      <form onSubmit={handleSubmit}>
-        <p>Введите имя</p>
-        <input
-          onChange={(e) =>
-            setNewUserObject({ ...newUserObject, userName: e.target.value })
-          }
-          placeholder="Имя"
-          type="text"
-        />
-        <p className="error-text">{errors.userName}</p>
-        <p>Введите фамилию</p>
-        <input
-          onChange={(e) =>
-            setNewUserObject({ ...newUserObject, sureName: e.target.value })
-          }
-          placeholder="Фамилия"
-          type="text"
-        />
-        <p className="error-text">{errors.sureName}</p>
-        <p>Введите Очество</p>
-        <input
-          onChange={(e) =>
-            setNewUserObject({ ...newUserObject, patronymic: e.target.value })
-          }
-          placeholder="Если есть"
-          type="text"
-        />
-        <p>Введите номер</p>
-        <IMaskInput
-          onChange={(e) =>
-            setNewUserObject({ ...newUserObject, telNumber: e.target.value })
-          }
-          mask="+{7} (000) 000-00-00"
-          placeholder="Введите номер телефона"
-        />
-        <p className="error-text">{errors.telNumber}</p>
-        <p>Введите почту</p>
-        <input
-          onChange={(e) =>
-            setNewUserObject({ ...newUserObject, email: e.target.value })
-          }
-          placeholder="Почта"
-          type="email"
-        />
-        <p className="error-text">{errors.email}</p>
-        <p>Выберите правильный город</p>
-        <div class="selectgorod">
-          <select
+    <div className="all-page">
+      <div className="left-content-page">
+        <form onSubmit={handleSubmit}>
+          <p>Введите имя</p>
+          <input
             onChange={(e) =>
-              setNewUserObject({ ...newUserObject, optionTown: e.target.value })
+              setNewUserObject({ ...newUserObject, userName: e.target.value })
             }
-          >
-            <option>Таганрог</option>
-            <option>Москва</option>
-            <option>Нью-Йорк</option>
-          </select>
+            placeholder="Имя"
+            type="text"
+          />
+          <p className="error-text">{errors.userName}</p>
+          <p>Введите фамилию</p>
+          <input
+            onChange={(e) =>
+              setNewUserObject({ ...newUserObject, sureName: e.target.value })
+            }
+            placeholder="Фамилия"
+            type="text"
+          />
+          <p className="error-text">{errors.sureName}</p>
+          <p>Введите Очество</p>
+          <input
+            onChange={(e) =>
+              setNewUserObject({ ...newUserObject, patronymic: e.target.value })
+            }
+            placeholder="Если есть"
+            type="text"
+          />
+          <p>Введите номер</p>
+          <IMaskInput
+            onChange={(e) =>
+              setNewUserObject({ ...newUserObject, telNumber: e.target.value })
+            }
+            mask="+{7} (000) 000-00-00"
+            placeholder="Введите номер телефона"
+          />
+          <p className="error-text">{errors.telNumber}</p>
+          <p>Введите почту</p>
+          <input
+            onChange={(e) =>
+              setNewUserObject({ ...newUserObject, email: e.target.value })
+            }
+            placeholder="Почта"
+            type="email"
+          />
+          <p className="error-text">{errors.email}</p>
+          <p>Выберите правильный город</p>
+          <div class="selectgorod">
+            <select
+              onChange={(e) =>
+                setNewUserObject({
+                  ...newUserObject,
+                  optionTown: e.target.value,
+                })
+              }
+            >
+              <option>Таганрог</option>
+              <option>Москва</option>
+              <option>Нью-Йорк</option>
+            </select>
+          </div>
+          <div>
+            <button type="submit">Отправить данные</button>
+          </div>
+        </form>
+      </div>
+      <div className="right-content-page">
+        <div className="first-block-list">
+          <div onKeyDown={keydownFunctions}>
+            {inputText.map((el) => {
+              return (
+                <p
+                  onClick={() => goida(el.id)}
+                  className={`${el.className} ${
+                    el.id === isActive ? activeStyle : ""
+                  }`}
+                >
+                  {el.text}
+                </p>
+              );
+            })}
+          </div>
         </div>
-        <div>
-          <button type="submit">Отправить данные</button>
+        <div className="second-block-list">
+          {/* {inputText.map((el) => {
+            return (
+              <p
+                key={el.id}
+                onClick={() => handleKeyDown(el.id)}
+                className={`${el.className} ${
+                  el.id === isActive ? activeStyle : ""
+                }`}
+              >
+                {el.text}
+              </p>
+            );
+          })} */}
         </div>
-      </form>
+      </div>
     </div>
   );
 };
---
+++ src/const/inputConst.jsx
@@ -0,0 +1,102 @@
+export const inputText = [
+  {
+    id: 1,
+    text: "1) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 2,
+    text: "2) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 3,
+    text: "3) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 4,
+    text: "4) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 5,
+    text: "5) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 6,
+    text: "6) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 7,
+    text: "7) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 8,
+    text: "8) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 9,
+    text: "9) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 10,
+    text: "10) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 11,
+    text: "11) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 12,
+    text: "12) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 13,
+    text: "13) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 14,
+    text: "14) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 15,
+    text: "15) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 16,
+    text: "16) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 17,
+    text: "17) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 18,
+    text: "18) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 19,
+    text: "19) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+  {
+    id: 20,
+    text: "20) Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eu tempora cumque! Maiores fugiat omnis rem ratione voluptatibus quo",
+    className: "el-text",
+  },
+];
